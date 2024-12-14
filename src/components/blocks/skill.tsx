fc9e2ede26b7d4dabb2946349ba6a267aacb6bb4
kotechi <aditiya53op@gmail.com>
2024-12-14 21:48:57 +0700

    apaweh

--- src/components/blocks/skill.tsx
+++ src/components/blocks/skill.tsx
@@ -22,66 +22,35 @@ export function Skill() {
       <div className="flex justify-center space-x-4 ">
 
         {/* Language */}
-        <div className="w-3/5 h-40 bg-gradient-to-t bg-[#ff00f225] hover:bg-[#ff00f238] border-4 border-[#81007B]  hover:border-[#ad00a5] rounded-md">
+        <div className="w-1/2 h-40 bg-gradient-to-t bg-[#ff00f225] hover:bg-[#ff00f238] border-4 border-[#81007B] hover:border-[#ad00a5] rounded-md">
           <div className="absolute m-3 px-2">
             <h1 className=" relative text-2xl font-bold">
-              <span className="z-20 opacity-75 text-[#af038a]">
-                Programming Language
-              </span>
+              <span className="z-20 opacity-75 text-[#af038a]">Programming language</span>
               <span className="z-10 absolute inset-0 text-white transform translate-y-1">
-                Programming Language
+              Programming language
               </span>
             </h1>
           </div>
-          <div className="flex space-x-3 w-full h-full pt-16 px-4">
-            <Language></Language>
+          <div className="flex items-center justify-center space-x-3 w-full h-full pt-16 px-4">
+          <Language></Language>
           </div>
+          
         </div>
 
         {/* DevOps */}
-        <div className="w-2/5 h-40 bg-gradient-to-t bg-[#ff00f225] hover:bg-[#ff00f238]  border-4 border-[#81007B] hover:border-[#ad00a5] rounded-md">
+        <div className="w-1/2 h-40 bg-gradient-to-t bg-[#ff00f225] hover:bg-[#ff00f238] border-4 border-[#81007B] hover:border-[#ad00a5] rounded-md">
           <div className="absolute m-3 px-2">
             <h1 className=" relative text-2xl font-bold">
               <span className="z-20 opacity-75 text-[#af038a]">DevOps</span>
               <span className="z-10 absolute inset-0 text-white transform translate-y-1">
-                DevOps
+              DevOps
               </span>
             </h1>
           </div>
-          <div className="flex space-x-3 w-full h-full pt-16 px-4">
-            <ul className="w-full h-full flex items-center text-white space-x-3">
-              <li className="flex flex-col justify-center">
-                <span className="flex justify-center">
-                  <img
-                    className="w-16 h-16 grayscale hover:grayscale-0"
-                    src="/images/Javascript-icon.png"
-                    alt=""
-                  />
-                </span>
-                <span>Javascript</span>
-              </li>
-              <li className="flex flex-col justify-center">
-                <span className="flex justify-center">
-                  <img
-                    className="w-16 h-16 grayscale hover:grayscale-0"
-                    src="/images/Javascript-icon.png"
-                    alt=""
-                  />
-                </span>
-                <span>Javascript</span>
-              </li>
-              <li className="flex flex-col justify-center">
-                <span className="flex justify-center">
-                  <img
-                    className="w-16 h-16 grayscale hover:grayscale-0"
-                    src="/images/Javascript-icon.png"
-                    alt=""
-                  />
-                </span>
-                <span>Javascript</span>
-              </li>
-            </ul>
+          <div className="flex items-center justify-center space-x-3 w-full h-full pt-16 px-4">
+          <DevOps></DevOps>
           </div>
+          
         </div>
       </div>
 
@@ -105,7 +74,7 @@ export function Skill() {
         </div>
 
         {/* Tools */}
-        <div className="w-1/2 h-40 bg-gradient-to-t bg-[#ff00f225] hover:bg-[#ff00f238]  border-4 border-[#81007B] hover:border-[#ad00a5] rounded-md">
+        <div className="w-1/2 h-40 bg-gradient-to-t bg-[#ff00f225] hover:bg-[#ff00f238] border-4 border-[#81007B] hover:border-[#ad00a5] rounded-md">
           <div className="absolute m-3 px-2">
             <h1 className=" relative text-2xl font-bold">
               <span className="z-20 opacity-75 text-[#af038a]">Tools</span>
@@ -117,6 +86,7 @@ export function Skill() {
           <div className="flex items-center justify-center space-x-3 w-full h-full pt-16 px-4">
           <Tools></Tools>
           </div>
+          
         </div>
       </div>
     </div>
